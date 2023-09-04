1f94923f0fcc8df9f3bb2f2ed1b0e7a044326200
abel vasconcelosb <78666074+abel8260@users.noreply.github.com>
2023-09-04 15:09:00 -0300

    mvp
    Produto Mínimo Viável (MVP)

---
+++ iodGH/iostreamDebug/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //for the std::cout
+
+int main()
+{
+    std::cout << "Hey there. ";
+    std::cout << "I am, currently, ";
+    std::cout << "using Whatsapp.";
+
+    return 0;
+
+}
